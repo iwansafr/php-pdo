c47b2356b5fdcd91a8d96e3323c9c34ffd94ee71
iwansafr <iwansafr@gmail.com>
2020-02-15 09:10:56 +0700

    function pr

--- index.php
+++ index.php
@@ -1,12 +1,11 @@
 <?php
-
+foreach(glob('sys/helper/*.php') as $file){
+	include $file;
+}
 include 'app/config/db.php';
 include 'sys/db/config.php';
 include 'sys/db/connection.php';
 include 'sys/route/route.php';
-foreach(glob('sys/helper/*.php') as $file){
-	include $file;
-}
 if(!empty($route->module()))
 {
 	$mod_path = 'app/modules/'.$route->module().'/';
